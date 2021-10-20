35fed0db37224883239c3b06d1520f8f8ad8c26e
Nimish Verma <nimishverma@ymail.com>
2021-10-19 22:22:43 -0400

    add: dynamodb functionality using serverless-lift and boto3

--- fastapi_serverless_sample/app.py
+++ fastapi_serverless_sample/app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from mangum import Mangum
 import os
-app = FastAPI(root_path="/dev/")
+import boto3
+
+
+app = FastAPI()
 
 stage = os.environ.get('STAGE', None)
-
 
 
 @app.get("/")
@@ -12,8 +14,17 @@ def index():
     return {"Hello": "World"}
 
 
-@app.get("/users/{user_id}")
-def read_item(item_id: int):
-    return {"user_id": item_id}
+@app.get("/users/user_id")
+def read_item(user_id: int):
+    table_name = os.environ.get('TABLE_NAME', '')
+    table = boto3.resource("dynamodb", region_name='us-west-2').Table(table_name)
+    response = table.get_item(
+        Key={
+            'PK': user_id
+        }
+    )
+
+    return {"user_obj": response['Item']}
+
 
 handler = Mangum(app)
